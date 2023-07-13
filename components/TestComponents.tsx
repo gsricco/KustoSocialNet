641c759f10ab0972457a6a7fb70f30337dd7637b
BogdanKurenkov <badikradik123@gmail.com>
2023-07-13 22:23:36 +0300

    feat: add eslint rules

---
+++ components/TestComponents.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+
+const TestComponents = () => {
+  return <div>TestComponents</div>;
+};
+
+export default TestComponents;
--- pages/jopa.ts
+++
@@ -1,5 +0,0 @@
-const count=()=>{
-    for(let i=0;i<20;i++){
-        console.log(i)
-    }
-}
