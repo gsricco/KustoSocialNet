d7f70b5f602e0e979f402a2c01488575ee1ba1ab
sergio <gsricco@yandex.ru>
2023-07-15 00:07:16 +0300

    fix: fixed success code

--- pages/auth/registration/success/index.tsx
+++ pages/auth/registration/success/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import {Button} from "../../../../common/components/Button/Button"
-import {StyledSignInWrapper, StyledText} from "../../../../styles/styledComponents/auth/FormikAuth.styled"
-import {WrapperContainerNoFrame} from "../../../../features/auth/WrapperContainerNoFrame"
+import {Button} from "@/common/components/Button/Button"
+import {StyledSignInWrapper, StyledText} from "@/styles/styledComponents/auth/FormikAuth.styled"
+import {WrapperContainerNoFrame} from "@/features/auth/WrapperContainerNoFrame"
 import VectorImage from "../../../../common/components/VectorImage"
 import {useRouter} from 'next/router';
 import mail from "../../../../public/img/icons/web-app-ui-sign-up-bro.svg";
@@ -9,20 +9,20 @@ import {
   StyledContainerAuth,
   StyledContainerButton,
   StyledImage
-} from "../../../../styles/styledComponents/auth/Auth.styled";
-import {getLayout} from "../../../../common/components/Layout/BaseLayout/BaseLayout";
+} from "@/styles/styledComponents/auth/Auth.styled";
+import {getLayout} from "@/common/components/Layout/BaseLayout/BaseLayout";
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import {GetStaticPropsContext} from "next"
-import config from 'next-i18next.config.js'
+import config from '@/next-i18next.config.js'
 import {useTranslation} from 'next-i18next'
-import {ThemeButton} from "../../../../common/enums/themeButton";
-import {Path} from "../../../../common/enums/path";
+import {ThemeButton} from "@/common/enums/themeButton";
+import {Path} from "@/common/enums/path";
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-  const {locale} = context as any
+  const {locale} = context
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"], config)),
+      ...(await serverSideTranslations(locale?locale:'', ["common"], config)),
     }
   }
 }
