f073cadd046e70e3d808f7e0ca1c58be46d70114
sergio <gsricco@yandex.ru>
2023-07-15 00:14:38 +0300

    fix: fixed registration verificationError code

--- pages/auth/registration/verificationError/index.tsx
+++ pages/auth/registration/verificationError/index.tsx
@@ -1,22 +1,23 @@
 import React, {useState} from "react"
-import {getLayout} from "../../../../common/components/Layout/BaseLayout/BaseLayout"
+import {getLayout} from "@/common/components/Layout/BaseLayout/BaseLayout"
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import {GetStaticPropsContext} from "next"
-import config from 'next-i18next.config.js'
+import config from '@/next-i18next.config.js'
 import {useTranslation} from 'next-i18next'
-import {useRefreshLinkMutation} from "../../../../assets/store/api/auth/authApi";
-import {Modal} from "../../../../common/components/Modal";
+import {useRefreshLinkMutation} from "@/assets/store/api/auth/authApi";
+import {Modal} from "@/common/components/Modal";
 import VerificationWindow from "../../../../features/auth/VerificationWindow";
-import { useLocalStorage } from "common/hooks/useLocalStorage"
+import { useLocalStorage } from "@/common/hooks/useLocalStorage"
 
 export async function getStaticProps(context: GetStaticPropsContext) {
-  const {locale} = context as any
+  const {locale} = context
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"], config)),
+      ...(await serverSideTranslations(locale?locale:'', ["common"], config)),
     }
   }
 }
+
 
 const Verification = () => {
   const [isModalActive, setIsModalActive] = useState(false)
